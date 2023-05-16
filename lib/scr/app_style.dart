4b377e91e3064419b36205a7dbfe4e9bf50bd810
Liew Jun Young <liewjunyoung@gmail.com>
2023-05-16 13:00:07 +0800

    added styles

---
+++ lib/scr/app_style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Style {
+  static const paddingHorizontal = EdgeInsets.symmetric(horizontal: 16.0);
+  static const sizedBoxSpace = SizedBox(height: 24);
+  static ButtonStyle fullWidthButton =
+      ElevatedButton.styleFrom(minimumSize: const Size.fromHeight(40));
+  //old
+  // static const cardPadding = SizedBox(width: 16, height: 16);
+  static const cardPadding = EdgeInsets.all(16);
+  static const modalPadding =
+      EdgeInsets.symmetric(horizontal: 20.0, vertical: 32);
+}
--- lib/track_theme.dart
+++ lib/track_theme.dart
@@ -2,3 +2,4 @@ library track_theme;
 
 export 'scr/app_color.dart';
 export 'scr/app_theme.dart';
+export 'scr/app_style.dart';
